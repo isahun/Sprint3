01764108458a765d4eb314b7f8b12a71170a7cb2
Irene Virgínia <isahun7@gmail.com>
2026-01-16 13:52:04 +0100

    fix: peer to peer corrections applied: ex1, removed console log // ex1, 2, 4, cleaned code, deleted return //ex4, 5 refactorized with arrow functions and ternary operator

--- src/films.js
+++ src/films.js
@@ -1,14 +1,11 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(movies) {
-  let result = movies.map(movie => movie.director);
-  console.log("EXERCISE 1 ->", result);
-  return result;
+  return movies.map(movie => movie.director);
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(movies, director) {
-  const result = movies.filter(movie => movie.director === director);
-  return result;
+  return movies.filter(movie => movie.director === director);
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
@@ -25,41 +22,19 @@ function moviesAverageOfDirector(movies, director) {
 }
 
 // Exercise 4:  Alphabetic order by title 
-function orderAlphabetically(movies) {
-  let orderedFilms = [...movies];
-  orderedFilms = orderedFilms.sort(function (a, b) {
-    if (a.title > b.title) {
-      return 1;
-    } else if (a.title < b.title) {
-      return -1
-    } else { return 0 }
-  });
-  
-  const result = orderedFilms.map(movie => movie.title).slice(0, 20);
-
-  return result;
+function orderAlphabetically (movies) { 
+  return [...movies].sort((a, b) => a.title > b.title ? 1 : a.title < b.title ? -1 : 0).map(movie => movie.title).slice(0, 20);
 }
 
 // Exercise 5: Order by year, ascending
 function orderByYear(movies) {
-  let orderedYear = [...movies];
-  
-  orderedYear.sort(function (a, b) {
-    if (a.year !== b.year) {
-      return a.year - b.year;
-    }
+  return [...movies].sort((a, b) => {
+    if (a.year !== b.year) return a.year - b.year;
 
-    if (a.title > b.title) {
-      return 1; 
-    } else if (a.title < b.title) { 
-      return -1; 
-    } else { 
-      return 0; 
-    }
+    return a.title > b.title ? 1 : a.title < b.title ? -1 : 0; 
   });
-
-  return orderedYear;
 }
+
 
 // Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory(movies, genre) {
@@ -78,8 +53,8 @@ function hoursToMinutes(movies) {
     let totalMins = 0;
 
     if (movie.duration.includes("h")) {
-      const movieHourToMins = parseInt(movie.duration)*60;
-      totalMins += movieHourToMins;
+      const hours = parseInt(movie.duration);
+      totalMins += isNaN(hours) ? 0 : hours * 60;
     }
 
     if (movie.duration.includes("min")) {
